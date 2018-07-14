e2f3e9fb45ffdbd454a9b861d6d15afd7b9ff6b4
Andrew Pollock <andrewpollock05@gmail.com>
2018-07-14 15:36:42 -0400

    This is not the final product

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friends = [
+	{
+		name: 'Donald Trump',
+		photo: 'https://static.ffx.io/images/$zoom_0.5787320601851852,$multiply_0.7554296506137866,$ratio_1.776846,$width_1059,$x_53,$y_0/t_crop_custom/t_sharpen,q_auto,f_auto/9bb382839d5724b35ced7e99a2bc5bffcf1f4ec1',
+		scores: [1, 1, 1, 5, 1, 5, 3, 5, 5, 5]
+	},{
+		name: 'Melania Trump',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5rAR9vrt2g7yfNFeSHLrBf8ZBSdWV2cGqd2TTh-w6mJzSfUyA2w',
+		scores: [5, 4, 5, 1, 3, 1, 1, 5, 1, 1]
+	},{
+		name: 'Ivanka Trump',
+		'https://i.pinimg.com/originals/90/fd/67/90fd6724dc4eddf176800a073d1038e0.jpg',
+		scores: [1, 5, 5, 1, 3, 4, 1, 5, 2, 1]
+	},{
+		name: 'Jared Kushner',
+		photo: 'https://queerty-prodweb.s3.amazonaws.com/2017/06/p056d4d3-670x377.jpg',
+		scores: [1, 4, 4, 5, 2, 5, 5, 3, 5, 5]
+    },{
+		name: 'Stephen Miller',
+		photo: 'https://socialnewsdaily.com/wp-content/uploads/2018/06/miller1.jpg',
+		scores: [1, 5, 5, 1, 1, 5, 5, 2, 5, 5]
+    },{
+		name: 'Sarah Sanders',
+		photo: 'https://cdn.pastemagazine.com/www/articles/Sarah%20Huckabee%20Sanders%20by%20Alex%20Wong%20Main.jpg',
+		scores: [3, 1, 5, 2, 3, 1, 1, 1, 1, 1]
+    },{
+		name: 'Kellyane Conway',
+		photo: 'https://media.vanityfair.com/photos/5ac4d14447083864d0b3beb2/master/pass/Kathy-Griffin-kellyanne-conway.jpg',
+		scores: [3, 4, 5, 5, 4, 1, 1, 5, 1, 1]
+    },{
+		name: 'Donald Trump Jr.',
+		photo: 'http://i.dailymail.co.uk/i/pix/2017/08/16/23/434F55FF00000578-4797416-Don_Jr_was_later_spotted_strolling_outside_after_an_hour_long_wo-m-56_1502923190171.jpg',
+		scores: [1, 5, 1, 5, 1, 5, 3, 5, 5, 5]
+    },{
+		name: 'Tiffany Trump',
+		photo: 'https://i.ytimg.com/vi/GxKtel5eY-Q/maxresdefault.jpg',
+		scores: [5, 5, 5, 5, 5, 3, 1, 4, 1, 1]
+    },{
+		name: 'Mike Pence',
+		photo: 'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1520174557/180304-avlon-mike-pence-hero_ey3rlw',
+		scores: [3, 1, 5, 1, 1, 1, 1, 1, 5, 5]
+    }];
+
+module.exports = friends;
